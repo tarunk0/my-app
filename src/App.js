5a5e6bd1501ed6e0d98a94d273d6f04298116b5a
Tarun Kumar <tarun.k0@yahoo.com>
2022-12-30 01:20:26 +0530

    jenkinsfile added

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-  This is my react JS  application.
+  This is my react JS  application. Fully Automated!
         </p>
         <a
           className="App-link"
